56ff8d004feac0fbcf384aea47da9ec9069e0af4
Ricardo Silva <ricardo10silva@gmail.com>
2018-11-02 17:14:45 +0000

    initial commit

---
+++ Assets/Scripts/IAJ.Unity/Pathfinding/Heuristics/EuclideanHeuristic.cs
@@ -0,0 +1,12 @@
+﻿using RAIN.Navigation.Graph;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Pathfinding.Heuristics
+{
+    public class EuclidianHeuristic : IHeuristic
+    {
+        public float H(NavigationGraphNode node, NavigationGraphNode goalNode) { 
+            return Vector3.Distance(goalNode.LocalPosition, node.LocalPosition);
+        }
+    }
+}
